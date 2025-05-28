1f120228bc77c91723dfd9db2655805445eb9e85
Tuyishime Evode <141217891+Judamrtn@users.noreply.github.com>
2025-05-28 11:26:35 +0200

    Update auth routes

--- routes/beneficiaryRoutes.js
+++ routes/beneficiaryRoutes.js
@@ -4,10 +4,10 @@ const controller = require('../controllers/beneficiaryController');
 const auth = require('../middlewares/authMiddleware');
 
 router.post('/registerBeneficially', controller.registerBeneficiary);
-router.post('/registerSponsorship', controller.registerSponsorship);
+router.post('/registerSponsorship', auth, controller.registerSponsorship); // 🔒 Protected
 router.post('/getBeneficially/:id', controller.getBeneficiaryById);
 router.get('/getBeneficialies', controller.getAllBeneficiaries);
 router.get('/getBeneficiaryStatus', controller.getSponsorships);
-router.patch('/updateBeneficialyStatus/:id', auth, controller.updateStatus); // protected
+router.patch('/updateBeneficialyStatus/:id', auth, controller.updateStatus); // 🔒 Protected
 
 module.exports = router;
